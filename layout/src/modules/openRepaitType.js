12cd1048484d59789fd21bd3da130f209887a2ab
Hotiakov <54938333+Hotiakov@users.noreply.github.com>
2021-09-13 14:48:37 +0300

    fix

--- layout/src/modules/openRepaitType.js
+++ layout/src/modules/openRepaitType.js
@@ -8,6 +8,7 @@ const openRepaitType = () => {
         link.addEventListener('click', e => {
             console.log(e.target);
             menu.classList.remove('active');
+            document.querySelector('.popup-menu').style.visibility = "hidden";
             popup.style.visibility = "inherit";
         });
     });
