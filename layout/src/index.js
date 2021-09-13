3081f6cf09d54a9fdfe94c33d93aadf02ee11a67
Hotiakov <54938333+Hotiakov@users.noreply.github.com>
2021-09-13 14:34:50 +0300

    done

--- layout/src/index.js
+++ layout/src/index.js
@@ -1,13 +1,17 @@
 import phoneDropdown from './modules/phoneDropdown';
 import burger from './modules/burger';
 import smoothScroll from './modules/smoothScroll';
+import openRepaitType from './modules/openRepaitType';
 
 document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
     //выпадающее меню телефона
     phoneDropdown();
     //Бургер-меню
     burger();
     //плавный скролл
     smoothScroll();
+    //Полный список услуг и цен
+    openRepaitType();
 
 });
---
+++ layout/src/modules/openRepaitType.js
@@ -0,0 +1,28 @@
+const openRepaitType = () => {
+    const popup = document.querySelector('.popup-repair-types'),
+        links = document.querySelectorAll('.link-list'),
+        menu = document.querySelector(".popup-dialog-menu"),
+        closeBtn = popup.querySelector('.close');
+
+    links.forEach(link => {
+        link.addEventListener('click', e => {
+            console.log(e.target);
+            menu.classList.remove('active');
+            popup.style.visibility = "inherit";
+        });
+    });
+
+    closeBtn.addEventListener('click', () => {
+        popup.style.visibility = "hidden";
+    });
+
+    popup.addEventListener('click', e => {
+        if(e.target === popup){
+            popup.style.visibility = "hidden";
+        }
+    });
+
+
+};
+
+export default openRepaitType;
