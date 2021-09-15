d3f03858235b27e5348e650b31f807f134deacb9
Hotiakov <54938333+Hotiakov@users.noreply.github.com>
2021-09-15 10:41:33 +0300

    formf ix

--- layout/src/modules/sendForm.js
+++ layout/src/modules/sendForm.js
@@ -1,10 +1,27 @@
 import makeRequest from "./makeRequest";
 
+const addNameValidation = input => {
+    input.addEventListener('blur', () => {
+        input.value = input.value.replace(/\s+/g, ' ')
+            .replace(/-+/g, '-')
+            .replace(/^\s/g, '')
+            .replace(/^-+/g, '');
+        input.value = input.value.replace(/((?<=-| )[а-яё]+|[а-яё]+)/gi, mathes => mathes[0].toUpperCase() + mathes.slice(1).toLowerCase());
+    });
+    input.addEventListener('input', () => {
+        input.value = input.value.replace(/[^а-яё\- ]/gi, '');
+    });
+}
+
+
 const sendForm = () => {
     const forms = document.querySelectorAll("form"),
         popupForm = document.querySelector('.popup-consultation'),
         popupThank = document.querySelector('.popup-thank');
     forms.forEach(form => {
+        const nameInput = form.querySelector("[name='name']");
+        if(nameInput)
+            addNameValidation(nameInput);
         form.addEventListener('submit', e => {
             e.preventDefault();
             const checkbox = form.querySelector('.checkbox__input');
@@ -32,6 +49,9 @@ const sendForm = () => {
                     popupForm.style.visibility = "hidden";
                     popupThank.style.visibility = "inherit";
                     form.reset();
+                    setTimeout(() => {
+                        popupThank.style.visibility = "hidden";
+                    }, 4000);
                     checkboxLabel.style.borderColor = 'black';
                 })
                 .catch((err) => {console.error(err); document.body.classList.remove('loaded');});
